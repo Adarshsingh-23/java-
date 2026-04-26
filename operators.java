812f90608821413d2750eaf7fa545f58b8848065
Adarshsingh-23 <sadarshpratap9066@gmail.com>
2026-04-26 20:48:34 +0530

    Add arithmetic, unary, and assignment operators examples

---
+++ operators.java
@@ -0,0 +1,20 @@
+// Arthmetic opertors//
+let a = 10;
+let b= 20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+//uniary operators
+ let c = 10;
+ let d = 5;
+ let sum= c+d;
+ console.log(sum++);
+ console.log(++sum);
+ console.log(sum--);
+ console.log(--sum);
+//Assignment opertors//
+ let f = 10;
+ let s = 20;
+  s = f;
+  console.log(s)
